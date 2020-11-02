9857a1b37c2ab6d78e0051b4e6963a416357f79c
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-02 21:04:06 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.35893db7b369377936c0cee9aa5cccbd.js"
 );
 
 self.addEventListener('message', (event) => {
